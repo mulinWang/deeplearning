024e696286eeb690958cd02fa6627397dfd9aeb6
wangsen <wangsen@ilongyuan.com.cn>
2017-10-31 20:13:20 +0800

    全连接神经网络 Node类，Connection类更新

---
+++ deeplearning-perceptron/src/test/scala/com/mulin/deeplearning/neuralnetwork/TestConnection.scala
@@ -0,0 +1,19 @@
+package com.mulin.deeplearning.neuralnetwork
+
+import org.scalatest.FunSuite
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.forkjoin.ThreadLocalRandom
+
+/**
+  * Created by mulin on 2017/10/31.
+  */
+class TestConnection extends FunSuite {
+  private[this] val log = LoggerFactory.getLogger(getClass)
+
+  test("test random") {
+    val weight: Float = ThreadLocalRandom.current().nextDouble(-1.0f, 1.0f).toFloat
+    log.info("weight: {}", weight)
+    assert(weight > -1.0f && weight < 1.0f)
+  }
+}
